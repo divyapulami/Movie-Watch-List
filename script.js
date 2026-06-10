2652d36adc4487ff661dd6cd955ae99c64be9def
divya <pulamidivya26@gmail.com>
2026-06-10 16:03:04 -0400

    phase 1: select DOM elements

---
+++ script.js
@@ -0,0 +1,48 @@
+const appTitle = document.getElementById("app-title")
+const movieCount = document.getElementById("movie-count")
+const movieForm = document.getElementById("movie-form")
+const titleInput = document.getElementById("title-input")
+const genreInput = document.getElementById("genre-input")
+const movieList = document.getElementById("movie-list")
+const clearWatchedBtn = document.getElementById("clear-watched-btn")
+
+
+// select #movie-form        → store in movieForm
+// select #title-input       → store in titleInput
+// select #genre-input       → store in genreInput
+// select #movie-list        → store in movieList
+// select #clear-watched-btn → store in clearWatchedBtn
+
+// select ALL elements with class "filter-btn" using querySelectorAll
+// store them in filterBtns — you'll loop over them in Phase 6
+const filterBtn = document.querySelectorAll(".filter-btn");
+const btnArray = Array.from(filterBtn);
+
+
+
+// select #movie-form        → store in movieForm
+
+// select #title-input       → store in titleInput
+// select #genre-input       → store in genreInput
+// select #movie-list        → store in movieList
+// select #clear-watched-btn → store in clearWatchedBtn
+
+// select ALL elements with class "filter-btn" using querySelectorAll
+// store them in filterBtns — you'll loop over them in Phase 6
+
+// select #movie-form        → store in movieForm
+// select #title-input       → store in titleInput
+// select #genre-input       → store in genreInput
+// select #movie-list        → store in movieList
+// select #clear-watched-btn → store in clearWatchedBtn
+
+// select ALL elements with class "filter-btn" using querySelectorAll
+// store them in filterBtns — you'll loop over them in Phase 6
+// select #movie-form        → store in movieForm
+// select #title-input       → store in titleInput
+// select #genre-input       → store in genreInput
+// select #movie-list        → store in movieList
+// select #clear-watched-btn → store in clearWatchedBtn
+
+// select ALL elements with class "filter-btn" using querySelectorAll
+// store them in filterBtns — you'll loop over them in Phase 6
